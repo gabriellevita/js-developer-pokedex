41f10511321b812fe8d3e9d1b90b69c668dbb2e0
Gabrielle de Vita <gabidevita18@gmail.com>
2023-02-26 21:05:11 -0300

    Update poke-api.js

--- js/poke-api.js
+++ js/poke-api.js
@@ -17,12 +17,3 @@ pokeApi.getPokemons = (limit=10, offset=0) => {
     .then((detailRequests) => Promise.all(detailRequests))
     .then((pokemonsDetails) => pokemonsDetails)
 }
-
-/* Promise.all([
-    fetch('https://pokeapi.co/api/v2/pokemon/1'),
-    fetch(`https://pokeapi.co/api/v2/pokemon/2`),
-    fetch(`https://pokeapi.co/api/v2/pokemon/3`),
-    fetch(`https://pokeapi.co/api/v2/pokemon/4`),
-]).then((results) => {
-    console.log(pokemonsDetails)
-}) */
